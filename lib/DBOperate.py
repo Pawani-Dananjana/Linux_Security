4be84b4bceeed50784317ad69614939cbd7dfbc5
Pawani-Dananjana <112768876+Pawani-Dananjana@users.noreply.github.com>
2023-11-27 15:12:50 +0530

    Update DBOperate.py

--- lib/DBOperate.py
+++ lib/DBOperate.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 filedb = {
     'PATH': '',
     'STAT': '',
@@ -17,64 +15,48 @@ recordmap = {
 
 def SQLcreatetable(c, tablename):
     try:
-        c.execute(f"SELECT * FROM {tablename} LIMIT 1;")
-        print(f"Table {tablename} already exists.")
-        return
-    except sqlite3.OperationalError:
+        c.execute('''DROP TABLE %s;''' % tablename)
+    except:
         pass
-
-    SQLCreat = f'''CREATE TABLE {tablename}
+    SQLCreat = '''CREATE TABLE %s
                (PATH  char(600) PRIMARY KEY  NOT NULL,
                STAT   char NOT NULL,
                MD5        char,
                Rule_Type  char,
                Rule_Check char,
                Record char(1));
-                  '''
+                  ''' % tablename
     c.execute(SQLCreat)
     print('Table created successfully', tablename)
 
 def SQLupdate(tablename, path, data: dict):
     str_ = ''
-    SQLUpdate1 = f"UPDATE {tablename} SET "
-    SQLUpdate2 = f"WHERE PATH='{path}'"
+    SQLUpdate1 = "UPDATE %s SET " % tablename
+    SQLUpdate2 = "WHERE PATH='%s'" % path
     for i in data:
-        str_ = str_ + f"{i}='{data[i]}',"
+        str_ = str_ + i + "='" + data[i] + "',"
     str_ = str_[:-1]
     SQLUpdate = SQLUpdate1 + str_ + SQLUpdate2
     return SQLUpdate
 
 def SQLinsert(tablename, data):
-    SQLInsert1 = f"INSERT INTO {tablename} "
+    SQLInsert1 = "INSERT INTO %s " % tablename
     SQLInsert2 = ''
     SQLInsert3 = ''
     for i in data:
-        SQLInsert2 = SQLInsert2 + f"{i},"
+        SQLInsert2 = SQLInsert2 + i + ','
     SQLInsert2 = SQLInsert2[:-1]
     for i in data:
-        SQLInsert3 = SQLInsert3 + f"'{data[i]}'" + ','
+        SQLInsert3 = SQLInsert3 + "'" + data[i] + "'" + ','
     SQLInsert3 = SQLInsert3[:-1]
-    SQLInsert = SQLInsert1 + f'({SQLInsert2})VALUES({SQLInsert3});'
+    SQLInsert = SQLInsert1 + '(' + SQLInsert2 + ')VALUES(' + SQLInsert3 + ');'
     return SQLInsert
 
 def queryFileData(c, tablename, path):
-    SQLQuery = f"SELECT * FROM {tablename} WHERE path='{path}'"
+    SQLQuery = "SELECT * FROM %s WHERE path=\'%s\'" % (tablename, path)
     c.execute(SQLQuery)
     values = c.fetchall()
     if values == []:
         return None
     else:
         return values[0]
-
-# Example usage
-if __name__ == "__main__":
-    # Connect to the database (replace 'your_database.db' with your actual database file)
-    conn = sqlite3.connect('init.db')
-    cursor = conn.cursor()
-
-    # Example: Create a table named 'FILEDB'
-    SQLcreatetable(cursor, 'FILEDB')
-
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
